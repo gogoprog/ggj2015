427bebf3e3c4aa61099aa04e46823569762d47a4
andreadst <andreadst@hotmail.com>
2015-01-24 00:22:21 +0100

    tweaked cam

--- settings.lua
+++ settings.lua
@@ -1,7 +1,7 @@
 Settings = Settings or {
     mapSize = 2048,
     worldRadius = 700,
-    cameraFactor = 750,
+    cameraFactor = -100,
     cameraSlowDown = 10,
     cameraRotateFactor = 0.5,
     clickableZone = 100
